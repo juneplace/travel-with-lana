5097b138d08d14a36a97d9a1152f9939313a0d94
Andrea Vaquero <avq40@outlook.es>
2021-06-24 16:16:17 +0200

    last changes

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-const reviews = [
-    {
-        id: 1,
-        pic: 
-        name: Anna Johnson,
+const lineOne = document.getElementById('line1');
+const lineTwo = document.getElementById('line2');
+const link = document.querySelector('.working-link');
+const linkDiv = document.querySelector('.working-div-link');
 
-    }
-]
+link.addEventListener('mouseover', function() {
+    linkDiv.classList.add('moving-lines')
+});
+link.addEventListener('mouseout', function() {
+    linkDiv.classList.remove('moving-lines')
+});
+const lines = anime({
+    targets: '.working-div-link',
+    direction: 'normal',
+    easing: 'spring(1, 80, 10, 0)'
+
+})
