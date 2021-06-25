bc09c55ff11e9f88334e7d5f64b544dc3617f719
Andrea Vaquero <avq40@outlook.es>
2021-06-25 18:36:24 +0200

    last changes

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-const lineOne = document.getElementById('line1');
-const lineTwo = document.getElementById('line2');
-const link = document.querySelector('.working-link');
-const linkDiv = document.querySelector('.working-div-link');
 
-link.addEventListener('mouseover', function() {
-    linkDiv.classList.add('moving-lines')
-});
-link.addEventListener('mouseout', function() {
-    linkDiv.classList.remove('moving-lines');
-});
 
 
   
@@ -16,16 +6,23 @@ const toggleBtn = document.querySelector(".sidebar-toggle");
 const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
 
-toggleBtn.addEventListener("click", function () {
-   if (sidebar.classList.contains("show-sidebar")) {
-    sidebar.classList.remove("show-sidebar")
-    sidebar.style.transform = 'translate(-100%)'
+document.addEventListener('DOMContentLoaded', function(){
+    sidebar.classList.remove("show-sidebar");
+    sidebar.classList.add("hide-sidebar");
+});
+
+toggleBtn.addEventListener('click', function () {
+   if(sidebar.classList.contains("show-sidebar")) {
+    sidebar.classList.remove("show-sidebar");
+    sidebar.classList.add("hide-sidebar");
    } else {
-     sidebar.classList.add('show-sidebar')
-     sidebar.style.transform = 'translate(0)'
+     sidebar.classList.add('show-sidebar');
+     sidebar.classList.remove('hide-sidebar')
+
    }
 });
 
-closeBtn.addEventListener("click", function () {
-  sidebar.classList.remove("show-sidebar")
+closeBtn.addEventListener('click', function () {
+  sidebar.classList.remove('show-sidebar');
+  sidebar.classList.add('hide-sidebar');
 });
