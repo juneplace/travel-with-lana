a4831e8090e9e24d99afa090d883594b13037159
Andrea Vaquero <avq40@outlook.es>
2021-06-25 18:15:28 +0200

    responsive design

--- app.js
+++ app.js
@@ -9,3 +9,23 @@ link.addEventListener('mouseover', function() {
 link.addEventListener('mouseout', function() {
     linkDiv.classList.remove('moving-lines');
 });
+
+
+  
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", function () {
+   if (sidebar.classList.contains("show-sidebar")) {
+    sidebar.classList.remove("show-sidebar")
+    sidebar.style.transform = 'translate(-100%)'
+   } else {
+     sidebar.classList.add('show-sidebar')
+     sidebar.style.transform = 'translate(0)'
+   }
+});
+
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar")
+});
